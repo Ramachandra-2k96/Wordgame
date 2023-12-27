4c639fe90f881d80b3645c9fb6491914cb722287
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-12-27 17:02:14 +0530

    major bugs fixed

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from .views import login_view, signup_view,chat
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('signup/', signup_view, name='signup'),
+    path('', signup_view, name='signup'),
     path('chat/', chat, name='chat'),
 ]
 
